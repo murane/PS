b3f47732de916c156b1fcb05c260c666aa9d7f6f
Junsu <murane@naver.com>
2020-09-11 01:12:24 +0900

    145 v1.0

--- Python/atcoder/abc145_d.py
+++ Python/atcoder/abc145_d.py
@@ -1,29 +1,28 @@
 import sys
-from collections import defaultdict
 r=sys.stdin.readline
 X,Y=map(int,r().split())
 #a=X-2b, b=Y-2a
-TB=defaultdict(int)
-sys.setrecursionlimit(10**6)
-TB[(0,1)]=1
-TB[(1,0)]=1
-if (2*Y-X)%3!=0 or (2*X-Y)%3!=0:
+if 2*X-Y<0 or (2*X-Y)%3!=0 or 2*Y-X<0 or (2*Y-X)%3!=0:
     print(0)
     exit(0)   
 a=(2*Y-X)//3
 b=(2*X-Y)//3
-mod=10**9+7
-#permutation with repetition
-#ex) ab, ba
-def dp(a,b):
-    if TB[(a,b)]!=0:
-        return TB[(a,b)]
-    if a==0 or b==0:
-        TB[(a,b)]=1
-        return TB[(a,b)]
-    else:
-        TB[(a,b)]=dp(a-1,b)+dp(a,b-1)
-        return TB[(a,b)]
-res = dp(a,b)
-print(res%mod) 
-    
+MOD=10**9+7
+#ex) ab, ba --> a+bCa
+def pow(a,b):
+    ans=1
+    while b:
+        if b&1:
+            ans=(ans*a)%MOD
+        a=(a**2)%MOD
+        b=b//2
+    return ans 
+N=a+b
+K=a
+fac=[1 for _ in range(N+1)]
+for i in range(2,N+1):
+    fac[i]=(fac[i-1]*i)%MOD
+A=fac[N]
+B=(fac[N-K]*fac[K])%MOD
+print((A%MOD)*pow(B,MOD-2)%MOD)
+
