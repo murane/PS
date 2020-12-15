69aa09896aa39a8ecb56108c6e7a7a730d4cf79e
Junsu <murane@naver.com>
2020-12-15 15:00:01 +0900

    12-15

--- Python/BOJ/test3.py
+++ Python/BOJ/test3.py
@@ -1,69 +1,36 @@
-from collections import deque
-def solution(board):
-    d=[(1,0),(-1,0),(0,1),(0,-1)]
-    l=len(board)
-    h_visit=[[-1]*(l-1) for _ in range(l)]#가로
-    v_visit=[[-1]*l for _ in range(l-1)]#세로
-    h_visit[0][0]=0
-    ans=0
-    q=deque()
-    q.append((0,0,True))#True는 가로 False는 세로
-    while q:
-        x,y,di=q.popleft()
-        if x==l-1 and y==l-2 and di:
-            ans=h_visit[x][y]
-            break
-        elif x==l-2 and y==l-1 and not di:
-            ans=v_visit[x][y]
-            break
-        for i in range(4):
-            Nx,Ny=x+d[i][0],y+d[i][1]
-            if di:
-                if not 0<=Nx<l or not 0<=Ny<l-1: continue
-                if board[Nx][Ny+1]!=0: continue
-                if h_visit[x][y]+1<=h_visit[Nx][Ny]:continue
-                print(f'{x},{y}:{h_visit[x][y]} -> {Nx},{Ny}:{h_visit[Nx][Ny]}')
-                h_visit[Nx][Ny]=h_visit[x][y]+1
-                q.append((Nx,Ny,True))
-            else:
-                if not 0<=Nx<l-1 or not 0<=Ny<l: continue
-                if board[Nx+1][Ny]!=0: continue
-                if v_visit[x][y]+1<=v_visit[Nx][Ny]:continue
-                v_visit[Nx][Ny]=v_visit[x][y]+1
-                q.append((Nx,Ny,False))
-        if di:#가로->세로
-            if x!=0 and board[x-1][y]==board[x-1][y+1]==0:
-                if h_visit[x][y]+1>v_visit[x-1][y]:
-                    v_visit[x-1][y]=h_visit[x][y]+1
-                    q.append((x-1,y,False))
-                if h_visit[x][y]+1>v_visit[x-1][y+1]:
-                    v_visit[x-1][y+1]=h_visit[x][y]+1
-                    q.append((x-1,y+1,False))
-            if x!=l-1 and board[x+1][y]==board[x+1][y+1]==0:
-                if h_visit[x][y]+1>v_visit[x][y]:
-                    v_visit[x][y]=h_visit[x][y]+1
-                    q.append((x,y,False))
-                if h_visit[x][y]+1>v_visit[x][y+1]:
-                    v_visit[x][y+1]=h_visit[x][y]+1
-                    q.append((x,y+1,False))
-        else:#세로->가로
-            if y!=0 and board[x][y-1]==board[x+1][y-1]==0:
-                if v_visit[x][y]+1>h_visit[x][y-1]:
-                    h_visit[x][y-1]=v_visit[x][y]+1
-                    q.append((x,y-1,True))
-                if v_visit[x][y]+1>h_visit[x+1][y-1]:
-                    h_visit[x+1][y-1]=v_visit[x][y]+1
-                    q.append((x+1,y-1,True))
-            if y!=l-1 and board[x][y+1]==board[x+1][y+1]==0:
-                if v_visit[x][y]+1>h_visit[x][y]:
-                    h_visit[x][y]=v_visit[x][y]+1
-                    q.append((x,y,True))
-                if v_visit[x][y]+1>h_visit[x+1][y]:
-                    h_visit[x+1][y]=v_visit[x][y]+1
-                    q.append((x+1,y,True))  
-    return ans
+from collections import defaultdict
+def is_match(pattern,word):
+    if pattern.startswith('?'):
+        for i in range(pattern.rfind('?')+1,len(word)):
+            if pattern[i]!=word[i]:
+                return False
+        return True
+    else:
+        for i in range(pattern.find('?')):
+            if pattern[i]!=word[i]:
+                return False
+        return True
+def solution(words, queries):
+    answer = []
+    res_tb=dict()
+    word_tb=defaultdict(list)
+    for word in words:
+        word_tb[len(word)].append(word)
+    for query in queries:
+        if query in res_tb:
+            answer.append(res_tb[query])
+            continue
+        tmp=0
+        for word in word_tb[len(query)]:
+            if is_match(query,word):
+                tmp+=1
+        res_tb[query]=tmp
+        answer.append(tmp)
+    return answer
 
 if __name__ == '__main__':
-    board=[[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]	
-    solution(board)
+    words=["frodo", "front", "frost", "frozen", "frame", "kakao"]
+    queries=["fro??", "????o", "fr???", "fro???", "pro?"]
+    solution(words,queries)
+
     
--- Python/BOJ/프랜즈4블록.py
+++ Python/BOJ/프랜즈4블록.py
@@ -1,36 +1,25 @@
-from string import ascii_uppercase
-def scan(m, n, tb)->list:
-    lst=set()
-    for i in range(m-1):
-        for j in range(n-1):
-            if tb[i][j]==tb[i+1][j+1]==tb[i+1][j]==tb[i][j+1] and tb[i][j] in ascii_uppercase:
-                lst.update([(i,j),(i,j+1),(i+1,j),(i+1,j+1)])
-    return list(lst)
-def down(m,n,board):
-    for col in range(n):
-        for i in range(m-1,-1,-1):
-            for j in range(m-1,0,-1):
-                if board[j][col]==" " and board[j-1][col] in ascii_uppercase:
-                    board[j][col],board[j-1][col]=board[j-1][col],board[j][col]
-    return board
-def solution(m, n, board):
+from collections import deque
+import heapq
+def solution(priorities, location):
     answer = 0
-    board=[list(row) for row in board]
-    #테이블에서 지워질 칸을 한번에 스캔하여 그 좌표들을 저장한다
+    lst=deque()
+    heap=[]
+    for i,v in enumerate(priorities):
+        lst.append((i,v))
+        heap.append(-v)
+    heapq.heapify(heap)
     while True:
-        lst=scan(m,n,board)
-        if not lst: break
-        #지운 칸의 갯수를 answer에 저장한다.
-        answer+=len(lst)
-        #한번에 칸들을 지우고 각 열별로 모든 블록을 바닥으로 내린다.
-        for x,y in lst:
-            board[x][y]=" "
-        board=down(m,n,board)
-    #지워질 칸이 없을때까지 반복한다.
+        if heap[0]==-lst[0][1]:
+            answer+=1
+            if lst[0][0]==location:
+                break
+            heapq.heappop(heap)
+            lst.popleft()
+        else:
+            lst.append(lst.popleft())
     return answer
 if __name__ == '__main__':
-    m=5
-    n=6
-    board=["AAAAAA", "BBAATB", "BBAATB", "JJJTAA", "JJJTAA"]
-    solution(m,n,board)
+    priorities=[1, 1, 9, 1, 1, 1]
+    location=0
+    solution(priorities,location)
     
