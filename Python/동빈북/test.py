290d7cfd904a1e6a94b75429992993e572187036
Junsu <murane@naver.com>
2021-01-11 18:31:24 +0900

    01-11

--- Python/동빈북/test.py
+++ Python/동빈북/test.py
@@ -1,26 +1,57 @@
-import heapq
-def solution(food_times, k):
-    heap=[]
-    if sum(food_times)<=k:
-        return -1
-    for idx,time in enumerate(food_times):
-        heapq.heappush(heap,(time,idx+1))
-    sum_val=0
-    prev=0
-    length=len(food_times)
-    while sum_val+(heap[0][0]-prev)*length<=k:
-        now=heapq.heappop(heap)[0]
-        sum_val+=(now-prev)*length
-        length-=1
-        prev=now
-    heap.sort(key=lambda x: x[1])
-    return heap[(k-sum_val)%length][1]
-
-if __name__ == '__main__':
-    food_times=[4,2,3,6,7,1,5,8]
-    k=27
-    #food_times=[3,1,2]
-    #k=1
-    answer = 5
-    print(solution(food_times, k))
-    
+import sys
+from collections import deque
+from copy import deepcopy
+r=sys.stdin.readline
+N=int(r())
+board,q=[],deque()
+ans=0
+for _ in range(N):
+    board.append(list(map(int,r().split())))
+def dfs(N):
+    global ans,board
+    if N==5:
+        ans=max(ans,max(map(max,board)))
+        return
+    tmpBoard=deepcopy(board)
+    for i in range(4):
+        move(i)
+        dfs(N+1)
+        board=deepcopy(tmpBoard)
+def get(i,j):
+    if board[i][j]:
+        q.append(board[i][j])
+        board[i][j]=0
+def merge(i,j,di,dj):
+    while q:
+        cur=q.popleft()
+        if not board[i][j]:
+            board[i][j]=cur
+        elif board[i][j]==cur:
+            board[i][j]=cur*2
+            i,j=i+di,j+dj
+        else:
+            i,j=i+di,j+dj
+            board[i][j]=cur
+def move(k):
+    if k == 0:
+        for j in range(N):
+            for i in range(N):
+                get(i, j)
+            merge(0, j, 1, 0)
+    elif k == 1:
+        for j in range(N):
+            for i in range(N-1, -1, -1):
+                get(i, j)
+            merge(N-1, j, -1, 0)
+    elif k == 2:
+        for i in range(N):
+            for j in range(N):
+                get(i, j)
+            merge(i, 0, 0, 1)
+    else:
+        for i in range(N):
+            for j in range(N-1, -1, -1):
+                get(i, j)
+            merge(i, N-1, 0, -1)
+dfs(0)   
+print(ans)
