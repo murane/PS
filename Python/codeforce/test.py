4b8e6b65b94bfb0860546e16551cee2617eadc45
jun <murane@naver.com>
2021-02-08 20:06:49 +0900

    02-08

--- Python/codeforce/test.py
+++ Python/codeforce/test.py
@@ -1,45 +1,30 @@
-n, m = map(int, input().split()) # voters, parties
- 
-money = [0] * n
- 
-for i in range(n):
-    x, y = map(int, input().split())
-    money[i] = (y, x - 1)
-    #cost, party
-money.sort()
- 
-def calc(lim):
-    visited = [False] * n
-    cost = 0
- 
-    cnt = [0] * m
-    for i in range(n):
-        cnt[money[i][1]] += 1
-        #파티별 득표수 체크
-    for i in range(n):
-        if money[i][1] != 0 and cnt[money[i][1]] >= lim:
-        #0번당이 아니고 득표가 lim보다 크거나 같으면..
-            cnt[money[i][1]] -= 1
-            cnt[0] += 1
-            cost += money[i][0]
-            visited[i] = True
-            #득표를 하나 줄이고 0번에 올린다음에.. 코스트를 하나 가져온다
-    for i in range(n):
-        if cnt[0] >= lim:
-            break
-        #한계치를 넘어서면 끝내자
-        if money[i][1] != 0 and not visited[i]:
-            cnt[money[i][1]] -= 1
-            cnt[0] += 1
-            cost += money[i][0]
-        #0번당이 아니고.. 방문을 안했으면..
-        
- 
-    return cost
- 
-ans = 10 ** 18
- 
-for i in range(n):
-    ans = min(ans, calc(i))
- 
-print(ans)
+import sys
+from collections import defaultdict
+def solution(n, s, a, b, fares):
+    INF=sys.maxsize
+    answer = INF
+    dist=[[INF]*(n+1) for _ in range(n+1)]
+    for start,end,cost in fares:
+        dist[start][end]=cost
+        dist[end][start]=cost
+    for i in range(1,n+1):
+        dist[i][i]=0
+    for m in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if dist[i][m]+dist[m][j]<dist[i][j]:
+                    dist[i][j]=dist[i][m]+dist[m][j]
+    def get_dist(mid):
+        return dist[s][mid]+dist[mid][a]+dist[mid][b]
+    for i in range(1,n+1):
+        answer=min(answer,get_dist(i))
+    return answer
+
+if __name__ == '__main__':
+    n=7
+    s=3
+    a=4
+    b=1
+    fares=[[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]
+    solution(n,s,a,b,fares)
+    
